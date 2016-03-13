13bd09accfebe732ccd3534cbdfa9ccd119517e3
nicholascm90 <nicholascm90@gmail.com>
2016-03-13 22:14:57 +0000

    abstracting out the loose functions into a URL manager class

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var app = new express();
 
 var validator = require('./js/validator.js'); 
 
+var DictionaryManager = require('./js/dictionaryManager.js'); 
+
+var dictionaryMgr = new DictionaryManager("https://fierce-everglades-29355.herokuapp.com/"); 
+
 var valid = new validator('https?:\/\/www\.[a-z]{1,100}\.[a-z]{2,3}.*'); 
 
 var baseAppUrl = "https://fierce-everglades-29355.herokuapp.com/"; 
@@ -15,6 +19,14 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {
     res.sendFile("index.html"); 
+}); 
+
+app.get('/test', function(req, res) {
+    var providedUrl = "test"; 
+    dictionaryMgr.addToDictionary(providedUrl); 
+    res.json(dictionaryMgr.alternativeUrls[dictionaryMgr.alternativeUrls.length-1]); 
+    console.log(dictionaryMgr.alternativeUrls); 
+
 }); 
 
 app.get('/*', function (req, res) {
@@ -59,7 +71,6 @@ app.listen(app.get('port'), function() {
 }); 
 
 
-//in memory database of URLs
 
 //TODO: all of the below should be abstracted into its own module for URL management and brought in as a dependency.
 
@@ -76,6 +87,8 @@ function checkForExisting(aSearchTerm, anArray) {
     }); 
     return exists; 
 }
+
+//searches for and provides an alternative url 
 
 function findAlternative(searchString) {
     var alternative = ""; 
---
+++ js/dictionaryManager.js
@@ -0,0 +1,55 @@
+
+function DictionaryManager(baseUrl) {
+    
+    this.baseUrl = baseUrl; 
+    
+    this.alternativeUrls = []; 
+    
+    this.count = 0; 
+    
+    //looks through the dictionary to see if a URL already exists for the provided url 
+
+    this.checkForExisting = function(aSearchTerm) {
+        var exists = false; 
+        
+        this.alternativeUrls.forEach(function(value) {
+            if (value.providedUrl == aSearchTerm) {
+                exists = true; 
+            }
+        }); 
+        
+        return exists; 
+    }; 
+    
+    //searches the list to check if an identical item in the dictionary already exists
+    
+    this.findAlternative = function(searchString) {
+        
+        var alternative = ""; 
+        
+        this.alternativeUrls.forEach(function(value) {
+            if (value.alternativeUrl == searchString) {
+                alternative = value.providedUrl; 
+            }
+        }); 
+        
+        return alternative; 
+    }; 
+    
+    this.addToDictionary = function(value) {
+      this.alternativeUrls.push({
+            providedUrl: value, 
+            alternativeUrl: this.baseUrl+this.getAlternativeUrl()
+        });  
+    }; 
+    
+    this.getAlternativeUrl = function() {
+        var alternativeUrl = this.count; 
+        this.count++; 
+        return alternativeUrl;  
+    }; 
+
+}
+
+module.exports = DictionaryManager; 
+
